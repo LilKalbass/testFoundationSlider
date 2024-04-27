4aefdf4c30215831badad02ed3d25b611058256a
Artem Dimoglov <adimoglov@gmail.com>
2024-04-27 12:59:35 +0300

    done

---
+++ components/testimSlider.jsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import {FreeMode, Navigation, Pagination, Thumbs} from 'swiper/modules'
+
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/thumbs'
+
+const Slider = ({clients}) => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null)
+    return (
+        <section className=''>
+            <div className='container mx-auto'>
+                <Swiper
+
+                    onSwiper={setThumbsSwiper}
+                    spaceBetween={20}
+                    slidesPerView={5}
+                    modules={[Thumbs]}
+                    watchSlidesVisibility={true}
+                    watchSlidesProgress={true}
+                    className='thumbs max-w-3xl rounded-lg'
+                >
+                    {clients.map((client, index) => (
+                        <SwiperSlide key={index}>
+                            {({isActive})=> (
+                                <div className='hidden md:flex p-4'>
+                                    <Image
+                                        src={client.avatar} width={120} height={120}
+                                        alt='qwe'
+                                        className= {`${isActive} : '' ? '' rounded-full`}
+                                    />
+                                </div>
+                                )}
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <Swiper
+                    loop={true}
+                    navigation={true}
+                    slidesPerView={1}
+                    initialSlide={2}
+                    thumbs={{
+                        swiper:
+                            thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+                    }}
+                    modules={[Navigation, Thumbs, Pagination]}
+                    pagination={{
+                        clickable:true,
+                        enabled:true,
+                }}
+                    className='mt-6'
+                    breakpoints={{
+                        420:{
+                            pagination: {
+                                clickable:true,
+                                enabled:true
+                            },
+                            navigation: {
+                                enabled:false
+                            }
+                        },
+                        680:{
+                            pagination: {
+                                clickable:false,
+                                enabled:false
+                            },
+                            navigation: {
+                                enabled:true
+                            }
+                        }
+                    }}
+                >
+                    {clients.map((client, index) => (
+                        <SwiperSlide key={index} className=''>
+                            <div className='flex items-center justify-center py-4 md:hidden'>
+                                <Image src={client.avatar} alt='qwe' width={120} height={120} className='rounded-full'/>
+                            </div>
+                            <div className='flex overflow-hidden w-full justify-center items-center  text-white flex-col xl:px-20 fl:px-12 text-center pt-4 pb-14'>
+                                <p className=' md:max-w-full max-w-[250px]'>{client.text}</p>
+                                <h3 className='xl:text-2xl text-xl fl:pt-4 xl:pt-2'>{client.name}</h3>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    )
+}
+
+export default Slider
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      fl: '360px',
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px'
+    },
+    colors: {
+      white: '#fff',
+    },
     extend: {
+      fontFamily: {
+
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        review: 'url(/assets/bg.jpg)',
+        reviewPhone: 'url(/assets/bgPhone.jpg)',
+      },
+      boxShadow: {
+        primary: '40px 4px 40px 0px rgba(68, 68, 68, 0.25)'
       },
     },
   },
-  plugins: [],
-};
+}
